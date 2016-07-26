f0040297c7b6221b5ae04f2b38a96c28d1c2ec2c
Erik Hellman <iam@erikhellman.com>
2016-07-26 17:04:32 +0200

    Create basic functions for lines and add animations

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,31 +1,99 @@
-console.log('Working!');
+var lines = 5;
+var amountOfLines = 5;
+const lineWidths = [
+  376, 291, 366, 184, 260, 188, 377, 242, 399, 194
+];
 
 const colors = [
-  'blue',
   'lightBlue',
   'purple',
   'lightPurple',
-  'orange',
+  'orange'
 ];
 
-const lineWidths = [
-  76, 91, 66, 84, 60, 88, 77, 42, 99, 94
+const orderedColors = [
+  'purple',
+  'lightBlue',
+  'purple',
+  'orange',
+  'purple',
+  'lightBlue',
+  'purple',
+  'lightBlue',
+  'purple',
+  'orange',
+  'purple',
+  'lightBlue',
+  'purple'
 ];
+
+function overwriteColorsWithOrdered(){
+  var lines = document.getElementsByClassName("line"),
+      len = lines !== null ? lines.length : 0,
+      i = 0;
+  for(i; i < len; i++) {
+      lines[i].className += " " + orderedColors[i];
+      // lines[i].className += " test";
+  }
+}
+function getRandomColor(){
+  var randomColor = colors[Math.floor(Math.random() * colors.length)];
+  return randomColor;
+}
+var image = document.getElementById('image');
+
+function createRandomLine(width, color){
+	// create line
+  var d = document.createElement('div');
+  // create circle
+  var c = document.createElement('div');
+  // set line width
+  d.setAttribute('style','width: ' + width + 'px');
+  // set line class
+  d.className = 'line ' + color;
+  // set circle class
+  c.className = 'circle';
+  // append circle to line
+  d.appendChild(c);
+  // append line to container
+  image.appendChild(d);
+}
+
+function createOrderedLine(){
+  // create line
+  var d = document.createElement('div');
+  // create circle
+  var c = document.createElement('div');
+  // set line width
+  d.setAttribute('style','width: ' + width + 'px');
+  // set line class
+  d.className = 'line ' + color;
+  // set circle class
+  c.className = 'circle';
+  // append circle to line
+  d.appendChild(c);
+  // append line to container
+  image.appendChild(d);
+
+
+}
+
+
+function displayOrderedLines(){
+  lineWidths.forEach(function(entry){
+    createRandomLine(entry, getRandomColor());
+  });
+}
 
 (function() {
-  var image = document.getElementById('image');
-  lineWidths.forEach(function(entry){
-    image.appendChild = '<div class="line" style="width: ' + entry + '"></div>';
-    console.log(entry);
-  });
-  // printLines();
-})();
 
-function printLines(){
-  var lines = 10;
-  lineWidths.forEach(function(entry){
-    image.innerHTML = '<div class="line" style="width: ' + entry + '"></div>';
-    console.log(entry);
-  });
-  // image.innerHTML = '<div class="line"></div>';
-}
+  displayOrderedLines();
+  var lines = document.getElementsByClassName("line"),
+      len = lines !== null ? lines.length : 0,
+      i = 0;
+  for(i; i < len; i++) {
+      lines[i].className += " animated slideInLeft";
+      // lines[i].className += " test";
+  }
+
+})();
